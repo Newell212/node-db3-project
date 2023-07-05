d5f382aa6ef54236017746547ac9fa64426fa86f
Michael Newell <michael.newell212@gmail.com>
2023-07-05 16:35:29 -0600

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -116,7 +116,15 @@ function find() { // EXERCISE A
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
+  const rows = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('step_number')
+
+    if(!rows[0].step_id) return []
+    return rows
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
